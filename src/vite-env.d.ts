aefd6aacd8b127f17b77f79acc6fdc373313bbd8
Suam Kang <ksa199653@gmail.com>
2023-09-05 00:57:24 +0900

    chore: .env 환경변수 타입 설정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+// 타입스크립트 환경에선, import.meta.env를 타입스크립트에서 기본적으로 제공하지 않기 떄문에,
+// ImportMetaEnv와 ImportMeta 인터페이스를 정의해주어야 한다.
+interface ImportMetaEnv {
+  readonly VITE_CUSTOM_ENV_VARIABLE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
