8211b8fdf73cd43f810399af250bf2745a49d57c
Suam Kang <ksa199653@gmail.com>
2023-09-14 18:20:27 +0900

    fix: 게시판 네브바 수정 및 리스트 아이템 정보 추가

--- src/components/board/BoardList.tsx
+++ src/components/board/BoardList.tsx
@@ -8,13 +8,14 @@ const BoardList = ({ posts }: { posts: PostProps[] }) => {
     <div className={classes.board__wrap}>
       <h1>자유롭게 이야기 하세요!</h1>
       <ul className={classes.board__list}>
-        {posts.map((post) => (
-          <li key={post.pid} className={classes.board__item}>
+        {posts.map((post, idx) => (
+          <li key={idx} className={classes.board__item}>
             <Link to={`${post.pid}`}>
               <img src={post.image} alt={post.title} />
               <div className={classes.content}>
                 <h2>{post.title}</h2>
                 <time>{post.date}</time>
+                <p>{post.userId}</p>
               </div>
             </Link>
           </li>
--- src/components/board/BoardNavbar.tsx
+++ src/components/board/BoardNavbar.tsx
@@ -1,7 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import classes from "./BoardNavbar.module.css";
+import { useAppSelector } from "../../redux/hooks";
 
 const BoardNavbar = () => {
+  const navigate = useNavigate();
+  const isLogin = useAppSelector((state) => state.auth.isLogin);
+
   return (
     <div className={classes.nav__inner}>
       <h2>
@@ -13,7 +17,18 @@ const BoardNavbar = () => {
             <Link to="/board">전체보기</Link>
           </li>
           <li>
-            <Link to="/board/new-post">글쓰기</Link>
+            {isLogin ? (
+              <Link to="/board/new-post">글쓰기</Link>
+            ) : (
+              <span
+                onClick={() => {
+                  alert("로그인 해야 이용 가능합니다.");
+                  navigate("/login");
+                }}
+              >
+                글쓰기
+              </span>
+            )}
           </li>
         </ul>
       </nav>
