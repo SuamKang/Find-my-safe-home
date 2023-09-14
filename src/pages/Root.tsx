f20f92756f1e5b69f2f1924edadefad897185a89
Suam Kang <ksa199653@gmail.com>
2023-09-14 18:16:59 +0900

    fix: 최상위 라우터에서 토큰 받는 방법 수정

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
-import { apiKey } from "../shared/firebase";
+// import { apiKey } from "../shared/firebase";
 
 import Header from "../components/layouts/header/Header";
 import Footer from "../components/layouts/footer/Footer";
@@ -13,19 +13,13 @@ const RootLayout = () => {
   const dispatch = useAppDispatch();
   const location = useLocation();
 
-  const _session_key = `firebase:authUser:${apiKey}:[DEFAULT]`;
-  // console.log(_session_key); // 로그인하면 있음
-  // console.log(sessionStorage.getItem(_session_key)); // null
-  const is_session = !!sessionStorage.getItem(_session_key);
-
   useEffect(() => {
     const token = getTokenFromSessionStorage();
 
-    if (token && is_session) {
-      // 토큰이 있고 세션에 인증이 있으면
+    if (token) {
       dispatch(asyncAuthActions.logInCheckFB());
     }
-  }, [dispatch, location.pathname]);
+  }, [location.pathname, dispatch]);
 
   return (
     <>
