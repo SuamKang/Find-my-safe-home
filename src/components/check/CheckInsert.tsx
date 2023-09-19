80f117d763639b36baf60b7acd4c947af89fc9e3
Suam Kang <ksa199653@gmail.com>
2023-09-19 16:10:02 +0900

    feat: 체크리스트 폼 양식 기능 추가

--- src/components/check/CheckInsert.tsx
+++ src/components/check/CheckInsert.tsx
@@ -1,10 +1,39 @@
+import { useState, FormEvent, ChangeEvent } from "react";
+
 import classes from "./CheckInsert.module.css";
 
+import { useAppDispatch } from "../../redux/hooks";
+import { asycnCheckActions } from "../../redux/actions/check-action";
+
 const CheckInsert = () => {
+  const dispatch = useAppDispatch();
+  const [inputValue, setInputValue] = useState("");
+
+  const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (inputValue) {
+      dispatch(asycnCheckActions.addCheckFB(inputValue));
+      setInputValue("");
+    }
+  };
+
   return (
-    <form className={classes.form}>
-      <label htmlFor="todo"></label>
-      <input id="todo" />
+    <form className={classes.form} onSubmit={submitHandler}>
+      <input
+        id="todo"
+        type="text"
+        name="text"
+        maxLength={15}
+        value={inputValue}
+        onChange={changeHandler}
+        placeholder="체크해야할 목록을 적어주세요.(최대 15자 내외)"
+        autoFocus
+      />
     </form>
   );
 };
