362e97ea071a57cfecc31ae40db122c2820be62e
Suam Kang <ksa199653@gmail.com>
2023-09-19 16:08:55 +0900

    fix: 게시글 작성자 검증 코드 추가

--- src/components/board/BoardItem.tsx
+++ src/components/board/BoardItem.tsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 import { PartialPost } from "../../shared/types";
-import { useAppDispatch } from "../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { asyncBoardActions } from "../../redux/actions/board-action";
 
 import classes from "./BoardItem.module.css";
@@ -11,10 +11,19 @@ const BoardItem = ({ post }: { post?: PartialPost }) => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
+  // 작성자만 본인 게시글 삭제 가능하도록 제한설정
+  const isRightUser = useAppSelector(
+    (state) => state.auth.user?.uid === post?.userId
+  );
+
   const removePostHandler = () => {
     const dobblecheck = window.confirm("정말 삭제하시겠습니까?");
 
-    if (dobblecheck) {
+    if (!isRightUser) {
+      alert("해당 게시글은 작성자만이 삭제 가능합니다!");
+    }
+
+    if (dobblecheck && isRightUser) {
       dispatch(asyncBoardActions.removePostFB(postId));
       navigate("/board");
     }
@@ -29,10 +38,14 @@ const BoardItem = ({ post }: { post?: PartialPost }) => {
         <p>{post?.description}</p>
       </div>
       <menu className={classes.post__actions}>
-        <Link to="edit-post">수정</Link>
-        <div id="button" onClick={removePostHandler}>
-          삭제
-        </div>
+        {isRightUser && (
+          <>
+            <Link to="edit-post">수정</Link>
+            <div id="button" onClick={removePostHandler}>
+              삭제
+            </div>
+          </>
+        )}
       </menu>
     </article>
   );
