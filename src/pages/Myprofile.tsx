b6286cac7b5f221d6e81b077e94f2587a7894116
Suam Kang <ksa199653@gmail.com>
2023-09-19 16:13:29 +0900

    chore: 프로필 컴포넌트 로그인시 연결로 수정

--- src/pages/Myprofile.tsx
+++ src/pages/Myprofile.tsx
@@ -1,18 +1,19 @@
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+import { useAppSelector } from "../redux/hooks";
 import PageContent from "../components/layouts/main/PageContent";
 import Profile from "../components/user-profile/Profile";
-import { useLocation } from "react-router-dom";
+
 const MyprofilePage = () => {
+  const isLogin = useAppSelector((state) => state.auth.isLogin);
+
   const { pathname } = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  return (
-    <PageContent>
-      <Profile />
-    </PageContent>
-  );
+  return <PageContent>{isLogin && <Profile />}</PageContent>;
 };
 
 export default MyprofilePage;
