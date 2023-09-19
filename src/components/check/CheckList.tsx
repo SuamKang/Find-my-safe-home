00680031ab960bfba609ded0415ab7e468d1cea6
Suam Kang <ksa199653@gmail.com>
2023-09-19 16:11:05 +0900

    feat: 체크리스트 리스트 리덕스툴킷 연결 및 기능추가

--- src/components/check/CheckList.tsx
+++ src/components/check/CheckList.tsx
@@ -1,16 +1,46 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import CheckInsert from "./CheckInsert";
 import CheckItem from "./CheckItem";
 import classes from "./CheckList.module.css";
+import { asycnCheckActions } from "../../redux/actions/check-action";
 
 // todo리스트 상태 관리 해야함!!!
+
+// check항목 하나에 들어있는
+// {
+//   userId,
+//   cid,
+//   text,
+//   done
+// }
+
 const CheckList = () => {
+  const todos = useAppSelector((state) => state.check.checks);
+  console.log(todos);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(asycnCheckActions.getCheckFB());
+  }, [dispatch]);
+
   return (
     <section className={classes.container}>
       <h1>체크리스트</h1>
       <CheckInsert />
-      <ul>
-        <CheckItem />
-      </ul>
+      <div className={classes.list}>
+        <ul>
+          {todos?.map((todo) => (
+            <CheckItem
+              key={todo.cid}
+              cid={todo.cid}
+              text={todo.text}
+              done={todo.done}
+            />
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
