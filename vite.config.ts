7e5e6a7386f1e4fd0b0ffcfc727e5c7dc278bfa2
Suam Kang <ksa199653@gmail.com>
2023-09-20 13:32:50 +0900

    chore: VIte 빌드 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // 빌드된 파일이 생성될 디렉터리
+    assetsDir: "assets", // 정적 자산 파일이 생성될 디렉터리
+  },
+});
