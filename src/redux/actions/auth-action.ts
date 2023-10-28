3acc5641cefd461cd0205673cd5a16f72efdbdbf
Suam Kang <ksa199653@gmail.com>
2023-10-28 17:13:29 +0900

    feat: 구글 로그인 엑션 생성자 함수 추가

--- src/redux/actions/auth-action.ts
+++ src/redux/actions/auth-action.ts
@@ -8,7 +8,10 @@ import {
   updateProfile,
   setPersistence,
   browserSessionPersistence,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth"; // 파이어베이스 인증에 필요한 메서드들
+import { app } from "../../shared/firebase";
 
 import {
   saveTokenToSessionStorage,
@@ -16,7 +19,7 @@ import {
   removeTokenFromSessionStorage,
 } from "../../shared/token";
 import { authActions } from "../slices/auth-slice";
-import { app } from "../../shared/firebase";
+
 import { AppDispatch } from "../index"; // type
 
 const { setUser, logout } = authActions;
@@ -86,6 +89,7 @@ export const logInFB = (enteredEmail: string, enteredPassword: string) => {
       if (error instanceof FirebaseError) {
         const errorCode = error.code;
         const errorMessage = error.message;
+        alert(errorMessage);
         console.log(errorCode, errorMessage);
       }
     }
@@ -129,7 +133,7 @@ export const checkRefreshToken = () => {
         if (user) {
           const refreshedToken = await user.getIdToken(true);
 
-          // 갱신된 토큰을 로컬 스토리지에 저장하기
+          // 갱신된 토큰을 세션 스토리지에 저장하기
           saveTokenToSessionStorage(refreshedToken);
 
           // 갱신된 토큰으로 사용자 정보 업데이트하기
@@ -161,8 +165,43 @@ const logOutFB = () => {
   };
 };
 
+// 구글로그인 엑션 생성자 함수
+const googleLoginFB = () => {
+  return async (dispatch: AppDispatch) => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth(app);
+
+    try {
+      const result = await signInWithPopup(auth, provider);
+      // "Cross-Origin-Opener-Policy policy would block the window.close call" 에러 발생
+      // 구글 로그인 팝업이 즉시 리디렉트되는 현상 사이에 연관성이 있을 수 있는거 같다. 이 오류는 브라우저의 보안 정책 중 하나로, 다른 도메인에서 열린 창 또는 프레임 간의 보안 문제를 검출하고 방지하기 위해 발생하게 된다. 즉 CORS설정이 필요한건데 좀 쉽게 접근하기 위해서 구글로그인 팝업 창을 키고 로그인이 완료되기도 전에 창이 닫히지 않도록 설정하게끔 로직을 구성하는 방법을 선택했다. -> login.tsx
+
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      const user = result.user;
+
+      saveTokenToSessionStorage(token as string);
+
+      // console.log(token?.slice(0, 5));
+      console.log(user);
+      const { email, displayName, uid, photoURL } = user;
+      const userInfo = { email, displayName, uid, photoURL };
+
+      dispatch(setUser(userInfo));
+    } catch (error: unknown) {
+      if (error instanceof FirebaseError) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData?.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(errorCode, errorMessage, email, credential);
+      }
+    }
+  };
+};
+
 // 회원 정보 수정 엑션 생성자 함수
-// 마이페이지에서 사용자 수정하기할때, 이름,이메일, 비밀번호, 사진을 수정가능하게 하는 함수를 구성해야함.
+// 마이페이지에서 사용자 정하기할때, 이름,이메일, 비밀번호, 사진을 수정가능하게 하는 함수를 구성해야함.
 
 // export const userProfileUpdatedFB = () => {}
 
@@ -173,4 +212,5 @@ export const asyncAuthActions = {
   checkRefreshToken,
   logInCheckFB,
   logOutFB,
+  googleLoginFB,
 };
