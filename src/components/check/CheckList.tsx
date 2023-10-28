7ab79ccd92b0042e7e504fb43c053072522c1238
Suam Kang <ksa199653@gmail.com>
2023-10-28 17:13:53 +0900

    fix: 유저 여부에 따른 리스트 렌더링 코드 수정

--- src/components/check/CheckList.tsx
+++ src/components/check/CheckList.tsx
@@ -6,10 +6,12 @@ import classes from "./CheckList.module.css";
 import { asycnCheckActions } from "../../redux/actions/check-action";
 
 const CheckList = () => {
-  const todos = useAppSelector((state) => state.check.checks);
-  console.log(todos);
-
   const dispatch = useAppDispatch();
+  // 해당 유저의 체크리스트 항목들만 가져오게 현재 로그인한 유저 정보 사용
+  const user = useAppSelector((state) => state.auth.user);
+  // console.log(user);
+  const todos = useAppSelector((state) => state.check.checks);
+  // console.log(todos);
 
   useEffect(() => {
     dispatch(asycnCheckActions.getCheckFB());
@@ -21,15 +23,17 @@ const CheckList = () => {
       <CheckInsert />
       <div className={classes.list}>
         <ul>
-          {todos?.map((todo) => (
-            <CheckItem
-              key={todo.cid}
-              userId={todo.userId}
-              cid={todo.cid}
-              text={todo.text}
-              done={todo.done}
-            />
-          ))}
+          {todos
+            ?.filter((todo) => todo.userId === user?.uid)
+            ?.map((todo) => (
+              <CheckItem
+                key={todo.cid}
+                userId={todo.userId}
+                cid={todo.cid}
+                text={todo.text}
+                done={todo.done}
+              />
+            ))}
         </ul>
       </div>
     </section>
